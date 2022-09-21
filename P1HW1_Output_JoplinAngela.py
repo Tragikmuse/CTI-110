3255cad2cd853e37af86c69356107129ae05821e
Tragikmuse <113923234+Tragikmuse@users.noreply.github.com>
2022-09-21 18:45:13 -0400

    Add files via upload

---
+++ P1HW1_Output_JoplinAngela.py
@@ -0,0 +1,10 @@
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num*user_num)
+print('And',user_num, 'cubed is', user_num*user_num*user_num, '!!')
+
+user_numtwo = int(input('Enter another integer:\n'))
+
+print(user_num , '+' , user_numtwo, 'is', user_num+user_numtwo)
+print(user_num , '*' , user_numtwo, 'is', user_num*user_numtwo)
