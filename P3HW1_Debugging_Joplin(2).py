183145f91c8f6cf9084f21a5f1db6776ec13b3c5
Tragikmuse <113923234+Tragikmuse@users.noreply.github.com>
2022-10-31 10:58:25 -0400

    Update P3HW1_Debugging_Joplin(2).py
    fixed psuedocode

--- P3HW1_Debugging_Joplin(2).py
+++ P3HW1_Debugging_Joplin(2).py
@@ -4,9 +4,11 @@
 # 10/24/2022
 
 
-# This program takes a number grade , determines average and displays letter grade for average.
-
-# Enter grades for six modules
+# Ask user to input 6 grades 
+# Add grades entered to a list
+# Calculate min, max, sum and average of grades
+# Display min, max, sum and average of grades
+# Determine and display letter grade for average
 
 mod_1 = float(input('Enter grade for Module 1: '))
 mod_2 = float(input('Enter grade for Module 2: '))
@@ -15,10 +17,7 @@ mod_4 = float(input('Enter grade for Module 4: '))
 mod_5 = float(input('Enter grade for Module 5: '))
 mod_6 = float(input('Enter grade for Module 6: '))
 
-# add grades entered to a list
-
 grades = [mod_1, mod_2, mod_3, mod_4, mod_5, mod_6]
-# TO DO: determine lowest, highest , sum and average for grades
 
 low = min(grades)
 high = max(grades)
@@ -34,7 +33,6 @@ print(f'Sum of Grades:    {sum_all:.2f}')
 print(f'Average:          {avg:.2f}')
 print('********************************')
 
-# determine letter grade for average
 
 if avg >= 90:
     print('Your grade is: A')
