eb51cbd7beac56d5223f5544d3216d7df581904e
Tragikmuse <113923234+Tragikmuse@users.noreply.github.com>
2022-10-31 10:37:06 -0400

    Update P3HW2_Salary_JoplinAngela.py
    input dollar signs and redid psuedocode

--- P3HW2_Salary_JoplinAngela.py
+++ P3HW2_Salary_JoplinAngela.py
@@ -3,10 +3,10 @@
 # Angela Joplin
 # 10/24/2022
 
-# Create a program that asks for an employee name, hours they have worked this month, and
-# their pay rate. The program should calculate if employee has overtime and if so what the
-# adjusted rate of pay will be and what regular pay is. It shoudl display, name, hours worked,
-# pay rate, OT hours, pay for OT, pay for regular hours, and gross pay.
+# Ask user for input for an employee name, hours they have worked, and their pay rate.
+# Calculate if employee has overtime and if so, what the adjusted rate of pay will be and what regular pay is.
+# Calculate gross pay given OT pay and regular pay.
+# Display, name, hours worked,pay rate, OT hours, pay for OT, pay for regular hours, and gross pay.
 
 #input
 name = (input("Enter employee's name: "))
@@ -33,4 +33,4 @@ print('Employee Name:  ',name)
 print()
 print('Hours Worked   Pay Rate   OverTime    OverTime Pay       Regular Pay      Gross Pay')
 print('**************************************************************************************')
-print(f'     {hours}          {payRate:.2f}        {ot}         {otPay:.2f}            {regPay:.2f}         {gross:.2f}')
+print(f'     {hours}          {payRate:.2f}        {ot}         ${otPay:.2f}            ${regPay:.2f}         ${gross:.2f}')
