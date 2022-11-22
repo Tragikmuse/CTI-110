f179c98a42fe2e8422865264b23b27ae67af0706
Tragikmuse <113923234+Tragikmuse@users.noreply.github.com>
2022-11-22 18:57:40 -0500

    Add files via upload

---
+++ P5LAB1_LeapYear_JoplinAngela.py
@@ -0,0 +1,14 @@
+#user_year = int(input('Enter year to determine the number of days in February that year: '))
+
+user_year = int(input())
+
+def days_in_feb(user_year):
+    if((user_year % 4 == 0 and user_year % 100 != 0) or (user_year % 400 == 0)):
+        print(f'{user_year} has 29 days in February.')
+    else:
+        print(f'{user_year} has 28 days in February.')
+
+days_in_feb(user_year)    
+
+
+
