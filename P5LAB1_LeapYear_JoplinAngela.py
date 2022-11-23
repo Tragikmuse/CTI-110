a200bf1496819ab34285a7c5d54051fc1f839df8
Tragikmuse <113923234+Tragikmuse@users.noreply.github.com>
2022-11-22 22:32:23 -0500

    Update P5LAB1_LeapYear_JoplinAngela.py

--- P5LAB1_LeapYear_JoplinAngela.py
+++ P5LAB1_LeapYear_JoplinAngela.py
@@ -35,4 +35,29 @@ if (days_in_feb(user_year)):
         print(f'{user_year} has 29 days in February.')
 else:
         print(f'{user_year} has 28 days in February.')
+'''        
+'''        
+def days_in_feb(user_year):
+    if (user_year % 4) == 0:
+        if (user_year % 100) == 0:
+            if (user_year % 400) == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+user_year = int(input())
+
+while (user_year) != 0:
+    if (days_in_feb(user_year)):
+        print(f'{user_year} has 29 days in February.')
+        user_year = int(input())
+    else:
+        print(f'{user_year} has 28 days in February.')
+        user_year = int(input())
+
+        
 '''
